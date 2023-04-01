bdf2dd50ae749d74172060c5cd57d845df645caf
francoMafferra <francomafferra38@gmail.com>
2023-04-01 02:36:49 -0300

    auth0 1ra parte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { LoginButton } from './components/Login/login'
+import { LogOutButton } from './components/Login/logOut'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <div className="App">
       <h1>E-Commerce</h1>
+      <LoginButton/>
+      <LogOutButton/>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Auth0Provider} from '@auth0/auth0-react'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain='dev-2llhwpnt06dhuqy3.us.auth0.com' 
+    clientId='dDyL1k58O24ODoBGnD2n3awYccV0Zccf'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
