116211983bf39c90563ab35f56f74fe46f2533c4
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-31 23:03:45 -0500

    Cambios SearchBar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { useDispatch } from "react-redux";
 import style from './SearchBar.module.css'
 
 const SearchBar = () => {
+
+    const dispatch = useDispatch;
+    const [ name, setName ] = useState("");
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        dispatch(GetGame(name))
+        setName('')
+    };
     return(
         <div className={style.containerSearchBar}>
             <div className={style.containerInput}>
-            <input type="text" placeholder="Search ..." />
-            <h3><IoIosSearch/></h3>
+            <input type="text" placeholder="Search ..." onChange={(e) => handleInputChange(e)} />
+            <button onClick={(e) => handleClick(e)}><IoIosSearch/></button>
             </div>
         </div>
     )
