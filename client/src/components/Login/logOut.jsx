1a999a16a2650dfb2a02724a1c86a0ed269281c2
Franco Mafferra <102844589+FrancoMafferra@users.noreply.github.com>
2023-04-01 03:49:01 -0300

    Revert "auth0 1ra parte"

--- client/src/components/Login/logOut.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-
-export const LogOutButton=()=> {
-  const { isLoading, isAuthenticated, error, user, loginWithPopup, logout} = useAuth0();
-  return <button onClick={() => logout()}>Log Out</button>;
-}
-
