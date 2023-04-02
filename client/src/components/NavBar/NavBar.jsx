f5f07599449fc4a6c802b5d23c0c332a1a384153
francoMafferra <francomafferra38@gmail.com>
2023-04-02 16:47:41 -0300

    varios estilos

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import style from './NavBar.module.css';
-import {AiOutlineShoppingCart} from "react-icons/ai"
+//import {BsBagCheckFill} from "react-icons/bs"
 import { useAuth0 } from '@auth0/auth0-react'
-import { Profile } from "../Profile/profile"; 
 import { LoginButton} from '../Login/login'
 import { LogOutButton } from '../Login/logOut'
+//import logo from "../../assets/logo/logo.png" no funciona!!
 
 
 const NavBar = () => {
@@ -12,16 +12,16 @@ const NavBar = () => {
     return(
         <div className={style.containerNavBar}>
             <div className={style.containerTitle}>
-                <h2>Tienda online</h2>
+                {/*<h2>{logo}</h2>*/}
+                <h2>Tienda Online</h2>
             </div>
             <div className={style.containerSearch}>
                 <div>
                 {isAuthenticated ?(<>
-        <Profile/>
         <LogOutButton/>
       </>):<LoginButton/>}
                 </div>
-                <h2><AiOutlineShoppingCart/></h2>
+                {/*<h2 className={style.logoCarritoCompras}><BsBagCheckFill/></h2>*/}
             </div>
         </div>
     )
