11b967118dd89967b78297c2a3217c215a58bef5
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-04-01 20:11:34 -0500

    Cambios Martin

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from './CardProduct.module.css'
 import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { getDog } from '../../Redux/Actions'
+// import { getDog } from '../../Redux/Actions'
 
 export const ProductCard = (props) => {
   const dispatch = useDispatch()
@@ -14,18 +14,15 @@ export const ProductCard = (props) => {
   }
   
   const productPrice = props.product.price ?  '$' + props.product.price  : ''
-  const productTitle = props.product.title ? props.dog.height_max : ''
+  const productName = props.product.type
 
-//   const dogWeightMin = props.dog.weight_min ? props.dog.weight_min + ' kg' : ''
-//   const dogWeightMax = props.dog.weight_max ? props.dog.weight_max + ' kg' : ''
-//   const dogWeight = dogWeightMin + (dogWeightMin && dogWeightMax && ' - ') + dogWeightMax
 
   return (
     
     <div className={style.productCardContainer} onClick={() => goToProductDetails()}>
       { props.dog.image && <img className={style.productImage} src={props.dog.image}/>}
       <div style={!props.product.image ? {paddingLeft: 20} : {}}>
-      <p className={style.productName}>{productTitle}</p>
+      <p className={style.productName}>{productName}</p>
       <p className={style.productHeight}>{productPrice}</p>
      
       </div>
