fbda4c8551890be8474590ca808afc3fdbc5dffe
Bach16 <gwyn56717@gmail.com>
2023-04-03 13:24:53 -0500

    fix development

--- client/src/components/Filters/SortByPrice.jsx
+++ client/src/components/Filters/SortByPrice.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import styles from "./ByType.css"
 import "./ByType.css"
 
 const SortByPrice = ({sortType,handleSelectChange}) => {
   return (
     <div style={{display:'flex',justifyContent:"center"}}>
         <select onChange={handleSelectChange} value={sortType} style={{marginTop:"10px"}} className='by-continent'>
-        <option value="asc" className={styles.option}>Menor precio</option>
-        <option value="desc" className={styles.option}>Mayor precio</option>
+        <option value="asc" className="option">Menor precio</option>
+        <option value="desc" className="option">Mayor precio</option>
       </select>
     </div>
   )
