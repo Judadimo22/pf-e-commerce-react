45b7dfb4302b249c0b314c91d77a43740daa40bf
francoMafferra <francomafferra38@gmail.com>
2023-04-06 01:30:23 -0300

    cambios listos a mejorar a futuro comentados

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -12,3 +12,20 @@ export const LoginButton = () => {
     </button>
   );
 };
+
+
+/*import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import style from "./log.module.css";
+
+const LoginButton = () => {
+  const { isLoading, isAuthenticated, error, user, loginWithPopup, logout } =
+    useAuth0();
+  console.log(isAuthenticated, user);
+  return (
+    <button className={style.buttonAuth} onClick={() => loginWithPopup()}>
+      Login
+    </button>
+  );
+};
+export default LoginButton*/
