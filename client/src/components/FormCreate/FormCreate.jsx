7c3efd16a5e8bf1f2ba3ec2202b826900f7b9be0
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-04-08 15:31:44 -0500

    Update FormCreate.jsx

--- client/src/components/FormCreate/FormCreate.jsx
+++ client/src/components/FormCreate/FormCreate.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "./formCreate.css
+import styles from "./formCreate.css"
 import {Link} from "react-router-dom"
 import {
   DriveFileRenameOutline,
