04eef63f6148bff33c35fae740508384ea30e689
Delamm <eldygonzalez.09@hotmail.com>
2023-04-12 04:12:56 -0300

    Empezando carrito

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,63 +1,76 @@
-import React from 'react'
-import style from './CardProduct.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import style from "./CardProduct.module.css";
+import { Link } from "react-router-dom";
 import { TbShoppingCartPlus } from "react-icons/tb";
-import { Box,  Button,  Flex, Image, Text } from '@chakra-ui/react'
-
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 
 export const ProductCard = (props) => {
-
- 
-
-
-  const productPrice = props.product.price ?  '$' + props.product.price  : ''
-  const productType = props.product.type
-  const productTrademark = props.product.trademark
-  const productImage = props.product.image
-  const productName = props.product.name
-
+  const productPrice = props.product.price ? "$" + props.product.price : "";
+  const productType = props.product.type;
+  const productTrademark = props.product.trademark;
+  const productImage = props.product.image;
+  const productName = props.product.name;
+  let products = [];
+  const handleClick = () => {
+    products.push({
+      image: props.product.image,
+      name: props.product.name,
+      price: props.product.price,
+      trademark: props.product.trademark,
+      type: props.product.type,
+    });
+    localStorage.setItem("cartItems", JSON.stringify(products));
+  };
 
   return (
-    <Flex flexDirection="column"  className={style.containerCard}>    
+    <Flex flexDirection="column" className={style.containerCard}>
+      <Box
+        className={style.boxContainer}
+        backgroundImage={productImage}
+        minH="320px"
+      >
+        <Link
+          style={{ color: "inherit", textDecoration: "inherit" }}
+          to={`/details/${props.product._id}`}
+        >
+          <Box height="260px" />
+        </Link>
 
-
-      <Box className={style.boxContainer} backgroundImage={productImage} minH="320px">
-
-        <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to={`/details/${props.product._id}`}>
-          <Box  height="260px" />
-        </Link>    
-
-        <Flex flexDir="row" >
-
-          <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to={`/details/${props.product._id}`}>
+        <Flex flexDir="row">
+          <Link
+            style={{ color: "inherit", textDecoration: "inherit" }}
+            to={`/details/${props.product._id}`}
+          >
             <Box width="190px" height="60px" />
-          </Link>    
+          </Link>
 
-          <Button className={style.cartContainer}  backgroundColor="#DAEB0F" padding="0">
-            <TbShoppingCartPlus className={style.cart}/>
+          <Button
+            className={style.cartContainer}
+            backgroundColor="#DAEB0F"
+            padding="0"
+          >
+            <TbShoppingCartPlus onClick={handleClick} className={style.cart} />
           </Button>
-
         </Flex>
-
       </Box>
 
-
       <Flex flexDir="column" margin="10px 0" textAlign="center">
-
-        <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to={`/details/${props.product._id}`}>
-          <Text className={style.productName} sx={{fontFamily:"Inter, sans-serif",fontWeight:"700"}} >{productName}</Text>
+        <Link
+          style={{ color: "inherit", textDecoration: "inherit" }}
+          to={`/details/${props.product._id}`}
+        >
+          <Text
+            className={style.productName}
+            sx={{ fontFamily: "Inter, sans-serif", fontWeight: "700" }}
+          >
+            {productName}
+          </Text>
         </Link>
 
         <Text className={style.productPrice}>{productPrice}</Text>
-
       </Flex>
-
     </Flex>
-  )
-}
-
-
-
-
+  );
+};
 
 // className={style.productCardContainer} onClick={() => goToProductDetails()}
--- client/src/components/NavBar/HomeNavbar.jsx
+++ client/src/components/NavBar/HomeNavbar.jsx
@@ -1,109 +1,133 @@
-import React, {useState, useEffect} from "react";
-import style from './NavBar.module.css';
+import React, { useState, useEffect } from "react";
+import style from "./NavBar.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import { LoginButton} from '../Login/login'
+import { LoginButton } from "../Login/login";
 import { createUser, getUserById } from "../../redux/actions";
-import SearchBar from '../SearchBar/SearchBar'
+import SearchBar from "../SearchBar/SearchBar";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { Avatar, Box, Button, Flex, Icon, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 
-
-
-
-
-
-
 const HomeNavBar = () => {
-    const dispatch = useDispatch(); 
-    const {isAuthenticated, user,logout} = useAuth0();
-      const [infoUser, setInfoUser] = useState({});
-      const userState = useSelector(state=>state.user)
+  const dispatch = useDispatch();
+  const { isAuthenticated, user, logout } = useAuth0();
+  const [infoUser, setInfoUser] = useState({});
+  const userState = useSelector((state) => state.user);
 
-    
-      useEffect(() => {
-        if (user && isAuthenticated) {
-          axios.get("https://backend-pf-uh1o.onrender.com/users").then((element) => {
-            const userDb = element.data.find(
-              (element) => element.email === user.email
-            );
-            if (!userDb) {
-              const newUser = {
-                name: user.given_name,
-                lastname: user.family_name,
-                email: user.email,
-              };
-    
-              console.log(newUser);
-              dispatch(createUser(newUser));
-            } else {
-              setInfoUser(userDb);
-            }
-            if(!userState.length) dispatch(getUserById(userDb._id))
-          });
-        }
+  useEffect(() => {
+    if (user && isAuthenticated) {
+      axios
+        .get("https://backend-pf-uh1o.onrender.com/users")
+        .then((element) => {
+          const userDb = element.data.find(
+            (element) => element.email === user.email
+          );
+          if (!userDb) {
+            const newUser = {
+              name: user.given_name,
+              lastname: user.family_name,
+              email: user.email,
+            };
 
-      }, [user]);
-      console.log(userState.roll);
-    
-    return(
+            console.log(newUser);
+            dispatch(createUser(newUser));
+          } else {
+            setInfoUser(userDb);
+          }
+          if (!userState.length) dispatch(getUserById(userDb._id));
+        });
+    }
+  }, [user]);
+  console.log(userState.roll);
+
+  return (
+    <>
+      <Flex h="70px" width="100%" />
+      <Flex className={style.containerNavBar} position="fixed" zIndex="9999">
         <>
-        <Flex h="70px" width="100%"/>
-        <Flex className={style.containerNavBar} position="fixed" zIndex="9999">
-
-
-
-        <>
-            <Link to={'/home'}><button>HOME</button></Link>
-            <Link to={'/about'}><button>ABOUT</button></Link>
-            <Link to={'/contact'}><button>CONTACT</button></Link>
-           
-              
-            </>
-
-
-
-
-            <Link to="/home">
-                <div className={style.containerTitle}>
-                    <Text sx={{fontSize: "50px",fontFamily:"Alumni Sans, sans-serif",fontWeight:"1000",marginLeft:"40px"}} >Ecommerce</Text>
-                </div>
-            </Link>
-            <SearchBar/>
-            <Flex w="8%" justifyContent="space-between">
-                {isAuthenticated ?(
-                <Menu cursor="pointer">
-                    <MenuButton >
-                        <Avatar src={user?.picture}  size="md"/>
-                    </MenuButton>
-                <MenuList >
-                    <Link to={`/user/${infoUser._id}`}>
-                        <MenuItem>Profile</MenuItem>
-                    </Link>
-                    <Link to={`/user/${infoUser._id}/orders`}>
-                        <MenuItem>My orders</MenuItem>
-                    </Link>
-                    <Link to={`/user/${infoUser._id}/notifications`}>
-                        <MenuItem>Notifications</MenuItem>
-                    </Link>
-                    {
-                      userState.roll === "admin" ? 
-                      (<Link to={`/admin`}>
-                        <MenuItem>Dashboard</MenuItem>
-                    </Link>) : null
-                    }
-
-                    <MenuItem onClick={()=>logout()}>Logout</MenuItem>
-                </MenuList>
-                </Menu>
-                ):<LoginButton/>}
-                <Icon bgColor="#f2f2f2" cursor="pointer" boxSize={12} borderRadius={50} p={2.5} justifyContent="center" alignItems="center"  as={AiOutlineShoppingCart}/> 
-            </Flex>
-        </Flex>
+          <Link to={"/home"}>
+            <button>HOME</button>
+          </Link>
+          <Link to={"/about"}>
+            <button>ABOUT</button>
+          </Link>
+          <Link to={"/contact"}>
+            <button>CONTACT</button>
+          </Link>
         </>
-    )
+
+        <Link to="/home">
+          <div className={style.containerTitle}>
+            <Text
+              sx={{
+                fontSize: "50px",
+                fontFamily: "Alumni Sans, sans-serif",
+                fontWeight: "1000",
+                marginLeft: "40px",
+              }}
+            >
+              Ecommerce
+            </Text>
+          </div>
+        </Link>
+        <SearchBar />
+        <Flex w="8%" justifyContent="space-between">
+          {isAuthenticated ? (
+            <Menu cursor="pointer">
+              <MenuButton>
+                <Avatar src={user?.picture} size="md" />
+              </MenuButton>
+              <MenuList>
+                <Link to={`/user/${infoUser._id}`}>
+                  <MenuItem>Profile</MenuItem>
+                </Link>
+                <Link to={`/user/${infoUser._id}/orders`}>
+                  <MenuItem>My orders</MenuItem>
+                </Link>
+                <Link to={`/user/${infoUser._id}/notifications`}>
+                  <MenuItem>Notifications</MenuItem>
+                </Link>
+                {userState.roll === "admin" ? (
+                  <Link to={`/admin`}>
+                    <MenuItem>Dashboard</MenuItem>
+                  </Link>
+                ) : null}
+
+                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          ) : (
+            <LoginButton />
+          )}
+          <Link to={`/cart`}>
+            <Icon
+              bgColor="#f2f2f2"
+              cursor="pointer"
+              boxSize={12}
+              borderRadius={50}
+              p={2.5}
+              justifyContent="center"
+              alignItems="center"
+              as={AiOutlineShoppingCart}
+            />
+          </Link>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default HomeNavBar;
