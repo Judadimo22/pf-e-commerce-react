ed681b459df5a94d9f49795eff54826624806f80
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-04-13 02:30:21 -0500

    cambios admin

--- client/cloudinary-browser.js
+++ client/cloudinary-browser.js
@@ -1,7 +1,7 @@
-import * as cloudinary from 'cloudinary-core';
+// import * as cloudinary from 'cloudinary-core';
 
-// Crea una nueva instancia de Cloudinary
-const cl = new cloudinary.Cloudinary({ cloud_name: 'dlqnb6csq' });
+// // Crea una nueva instancia de Cloudinary
+// const cl = new cloudinary.Cloudinary({ cloud_name: 'dlqnb6csq' });
 
-// Exporta la instancia para poder ser usada en otros archivos
-export default cl;
+// // Exporta la instancia para poder ser usada en otros archivos
+// export default cl;
