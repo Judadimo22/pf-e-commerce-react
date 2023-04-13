f2b9dc5e11f9735ab528a01daa4a519b4391f2c6
Delamm <eldygonzalez.09@hotmail.com>
2023-04-12 21:49:17 -0300

    modificando ruta de back de mercado pago para carrito

--- api/src/controllers/mpController.js
+++ api/src/controllers/mpController.js
@@ -3,6 +3,7 @@ const productSchema = require("../models/Product");
 
 const pagarProducto = async (req, res) => {
   const product = req.body.dataMP;
+  const productos = req.body.dataMP.id;
   const email = product.email;
   const productId = await productSchema.findById(product.id);
 
@@ -27,6 +28,18 @@ const pagarProducto = async (req, res) => {
     auto_return: "approved",
     external_reference: email,
   };
+
+  productos.forEach((e) => {
+    preference.items.push({
+      category_id: e.id,
+      title: e.name,
+      description: e.description,
+      picture_url: e.image,
+      quantity: e.quantity,
+      currency_id: "ARS",
+      unit_price: e.price,
+    });
+  });
 
   mercadopago.preferences
     .create(preference)
