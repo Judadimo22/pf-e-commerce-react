321e1b7a39621321d86d0f8bafbab8e46aefb40f
Bach16 <gwyn56717@gmail.com>
2023-04-14 23:09:00 -0500

    add new nav item

--- client/src/views/User/UserPage.jsx
+++ client/src/views/User/UserPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import HomeNavBar from '../../components/NavBar/HomeNavbar'
 import { DashboardLeftMenu } from '../../components/SideMenu/SideMenu'
 import { TbRuler, TbTruckDelivery, TbUserCircle } from 'react-icons/tb'
-import { HiOutlineMail } from 'react-icons/hi'
+import { HiOutlineLocationMarker, HiOutlineMail } from 'react-icons/hi'
 import { Flex } from '@chakra-ui/react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -22,21 +22,26 @@ const UserPage = () => {
   },[])
 
     const nav= [
-        {
-            icon:TbUserCircle,
-            title:"General",
-            redirect:`/user/${id}`
-        },
-        {
-            icon:TbTruckDelivery,
-            title:"My orders",
-            redirect:`/user/${id}/orders`
-        },
-        {
-            icon:HiOutlineMail,
-            title:"Notifications",
-            redirect:`/user/${id}/notifications`
-        },
+      {
+        icon:TbUserCircle,
+        title:"General",
+        redirect:`/user/${id}`
+      },
+      {
+          icon:TbTruckDelivery,
+          title:"My orders",
+          redirect:`/user/${id}/orders`
+      },
+      {
+          icon:HiOutlineMail,
+          title:"Notifications",
+          redirect:`/user/${id}/notifications`
+      },
+      {
+          icon:HiOutlineLocationMarker,
+          title:"Addresses",
+          redirect:`/user/${id}/addresses`
+      },
     ]
 
   return (
