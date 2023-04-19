0a56f1efec85d82798ac7fe6b572624ab724e2c2
Delamm <eldygonzalez.09@hotmail.com>
2023-04-19 19:05:23 -0300

    Update detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -15,9 +15,17 @@ import ClothReviews from "../Reviews/clothReviews";
 export const Details = () => {
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.Details);
+  console.log("productDetails", productDetails.tallas);
+  let sizes = productDetails.tallas;
+  console.log(sizes);
   const { id } = useParams();
-  const { name, trademark, description, image, price, size, review, stock } =
+
+  const { name, trademark, description, image, price, tallas, review, stock } =
     productDetails;
+  // const tallas = productDetails.tallas;
+  // const totalStock = productDetails.tallas.reduce((total, talla) => {
+  //   return total + talla.stock;
+  // }, 0);
 
   const { isAuthenticated, user } = useAuth0();
   const [pay, setPay] = useState(false);
@@ -39,6 +47,7 @@ export const Details = () => {
   }, []);
 
   const [selectedSize, setSelectedSize] = useState("");
+  console.log("selectedSize", selectedSize);
 
   const handleSizeSelect = (size) => {
     setSelectedSize(size);
@@ -87,7 +96,9 @@ export const Details = () => {
     localStorage.setItem("cartItems", JSON.stringify(cart));
 
     // Actualizar el estado del número de productos en el carrito
-    //setNumCartItems(cart.length);
+
+    setNumCartItems(cart.length);
+    setSelectedSize("");
   };
 
   return (
@@ -124,14 +135,17 @@ export const Details = () => {
               <Chakra.Text ml={1}>(1 review)</Chakra.Text>{" "}
               {/* que alguien añada reviews o rompo development */}
             </Chakra.Flex>
-            <Chakra.Text fontWeight="bold" fontSize="lg" pb="1rem">
-              Seleccionar Talle
-            </Chakra.Text>
-            <SizeSelector
-              sizes={size}
-              selectedSize={selectedSize}
-              onSizeSelect={handleSizeSelect}
-            />
+            <Chakra.Flex>
+              {sizes?.map((talla) => (
+                <SizeSelector
+                  size={talla.talla}
+                  stock={talla.stock}
+                  key={talla._id}
+                  selectedSize={selectedSize}
+                  onSizeSelect={handleSizeSelect}
+                />
+              ))}
+            </Chakra.Flex>
             <Chakra.Text fontWeight="bold" fontSize="lg" pt="1.5rem" pb="1rem">
               Colores Disponibles
             </Chakra.Text>
@@ -157,6 +171,7 @@ export const Details = () => {
                 >
                   Agregar al carrito
                 </Chakra.Button>
+
                 <Chakra.Text
                   fontSize="md"
                   fontWeight="bold"
@@ -164,7 +179,11 @@ export const Details = () => {
                   pt=".7rem"
                   color="#565656"
                 >
-                  {stock} Disponibles
+                  {selectedSize === ""
+                    ? "Selecciona una talla"
+                    : productDetails.tallas.find(
+                        (t) => t.talla === selectedSize
+                      ).stock + " Disponibles"}
                 </Chakra.Text>
               </Chakra.Flex>
             </Chakra.Box>
@@ -195,3 +214,201 @@ export const Details = () => {
     </>
   );
 };
+
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Link, useParams } from "react-router-dom";
+// import { getClothById } from "../../redux/actions";
+// import HomeNavBar from "../NavBar/HomeNavbar";
+// import * as Chakra from "@chakra-ui/react";
+// import StarRating from "../StarRating/StarRating";
+// import SizeSelector from "../SizeSelector/SizeSelector";
+// import ColorSelector from "../ColorSelector/ColorSelector";
+// import { FaShoppingCart } from "react-icons/fa";
+// import Footer from "../Footer/Footer";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import ClothReviews from "../Reviews/clothReviews";
+
+// export const Details = () => {
+//   const dispatch = useDispatch();
+//   const productDetails = useSelector((state) => state.Details);
+//   const { id } = useParams();
+//   const { name, trademark, description, image, price, tallas, review, stock } =
+//     productDetails;
+
+//   const { isAuthenticated, user } = useAuth0();
+//   const [pay, setPay] = useState(false);
+//   const allData = useSelector((state) => state.Details);
+
+//   function handlerPay() {
+//     setPay(true);
+//     Swal.fire({
+//       position: "center",
+//       icon: "warning",
+//       title: "You need a login",
+//       showConfirmButton: true,
+//       //timer: 3000,
+//     });
+//   }
+
+//   useEffect(() => {
+//     dispatch(getClothById(id));
+//   }, []);
+
+//   const [selectedSize, setSelectedSize] = useState("");
+
+//   const handleSizeSelect = (size) => {
+//     setSelectedSize(size);
+//   };
+
+//   const [selectedColor, setSelectedColor] = useState("#000");
+//   const colors = ["#000", "#FFF", "#FF5733", "#2ECC71", "#3498DB"];
+
+//   const handleColorSelect = (color) => {
+//     setSelectedColor(color);
+//   };
+
+//   const handleAddToCart = () => {
+//     // Obtener los datos del producto seleccionado
+//     const product = {
+//       id: productDetails.id,
+//       name: productDetails.name,
+//       price: productDetails.price,
+//       size: selectedSize,
+//       color: selectedColor,
+//       image: productDetails.image,
+//       quantity: 1,
+//     };
+
+//     // Obtener el carrito actual desde el localStorage
+//     let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+//     // Verificar si el producto ya está en el carrito
+//     const existingProductIndex = cart.findIndex(
+//       (item) =>
+//         item.id === product.id &&
+//         item.size === product.size &&
+//         item.color === product.color &&
+//         item.image === product.image
+//     );
+
+//     if (existingProductIndex >= 0) {
+//       // Si el producto ya está en el carrito, aumentar la cantidad
+//       cart[existingProductIndex].quantity += 1;
+//     } else {
+//       // Si el producto no está en el carrito, agregarlo
+//       cart.push(product);
+//     }
+
+//     // Actualizar el carrito en el localStorage
+//     localStorage.setItem("cartItems", JSON.stringify(cart));
+
+//     // Actualizar el estado del número de productos en el carrito
+//     //setNumCartItems(cart.length);
+//   };
+
+//   return (
+//     <>
+//       <HomeNavBar />
+//       <Chakra.Box h="100%" pt="1rem">
+//         <Chakra.HStack spacing={4}>
+//           <Link to={`/home`}>
+//             <Chakra.Box m={3} pl="2rem" pb=".5rem">
+//               <Chakra.Text>Home {`>`}</Chakra.Text>
+//               <Chakra.Box h="1px" w="100%" bg="gray" borderRadius="full" />
+//             </Chakra.Box>
+//           </Link>
+//         </Chakra.HStack>
+//         <Chakra.Grid templateColumns={["1fr", "1fr", "repeat(2, 1fr)"]} gap={6}>
+//           <Chakra.Box>
+//             <Chakra.Image
+//               src={image}
+//               alt={name}
+//               borderRadius="10px"
+//               mx="auto"
+//             />
+//           </Chakra.Box>
+//           <Chakra.Box>
+//             <Chakra.Text fontSize="4xl" fontWeight="extrabold" mb={3} mt={-4}>
+//               {name}
+//             </Chakra.Text>
+//             <Chakra.Text fontSize="3xl" fontWeight="bold" pb="1.5rem">
+//               ${price}.00
+//             </Chakra.Text>
+//             <Chakra.Box h="1px" w="95%" bg="gray" borderRadius="full" />
+//             <Chakra.Flex pt="1.5rem" pb="2rem">
+//               <StarRating rating={4.5} />
+//               <Chakra.Text ml={1}>(1 review)</Chakra.Text>{" "}
+//               {/* que alguien añada reviews o rompo development */}
+//             </Chakra.Flex>
+//             <Chakra.Text fontWeight="bold" fontSize="lg" pb="1rem">
+//               Seleccionar Talle
+//             </Chakra.Text>
+//             <SizeSelector
+//               sizes={size}
+//               selectedSize={selectedSize}
+//               onSizeSelect={handleSizeSelect}
+//             />
+//             <Chakra.Text fontWeight="bold" fontSize="lg" pt="1.5rem" pb="1rem">
+//               Colores Disponibles
+//             </Chakra.Text>
+//             <ColorSelector
+//               colors={colors}
+//               selectedColor={selectedColor}
+//               onColorSelect={handleColorSelect}
+//             />
+//             <Chakra.Box pt="1.5rem" pb="1.5rem" mt={8} mb={4}>
+//               <Chakra.Flex>
+//                 <Chakra.Button
+//                   borderRadius="100px"
+//                   colorScheme="#DAEB0F"
+//                   bg="#DAEB0F"
+//                   size="lg"
+//                   pl="3rem"
+//                   pr="3rem"
+//                   _hover="white"
+//                   color="#272727"
+//                   disabled={!selectedSize || !selectedColor}
+//                   onClick={handleAddToCart}
+//                   leftIcon={<FaShoppingCart />}
+//                 >
+//                   Agregar al carrito
+//                 </Chakra.Button>
+//                 <Chakra.Text
+//                   fontSize="md"
+//                   fontWeight="bold"
+//                   pl="2rem"
+//                   pt=".7rem"
+//                   color="#565656"
+//                 >
+//                   {stock} Disponibles
+//                 </Chakra.Text>
+//               </Chakra.Flex>
+//             </Chakra.Box>
+//             <Chakra.Box h="1px" w="95%" bg="gray" borderRadius="full" />
+//             <Chakra.Text
+//               fontSize="md"
+//               fontWeight="normal"
+//               mb={1}
+//               pt="1.5rem"
+//               color="#565656"
+//             >
+//               Marca {trademark ? trademark : "N/A"}
+//             </Chakra.Text>
+//             <Chakra.Text
+//               fontSize="md"
+//               fontWeight="normal"
+//               mb={1}
+//               color="#565656"
+//               pb="5rem"
+//             >
+//               {description ? description : "N/A"}
+//             </Chakra.Text>
+//           </Chakra.Box>
+//         </Chakra.Grid>
+//       </Chakra.Box>
+//       <ClothReviews id={id} comment={allData.review} />
+//       <Footer />
+//     </>
+//   );
+// };
