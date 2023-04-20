a2163cb1cebf1c31581ee1917e70c504092b6b07
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-04-19 21:12:15 -0500

    admin navBar responsive

--- client/src/components/NavBar/AdminNavBar.jsx
+++ client/src/components/NavBar/AdminNavBar.jsx
@@ -23,7 +23,7 @@ const AdminNavBar = () => {
         <Flex className={style.containerNavBar}>            
             <Link to="/home">
                 <div className={style.containerTitle}>
-                    <Text sx={{fontSize: "50px",fontFamily:"Alumni Sans, sans-serif",fontWeight:"1000",marginLeft:"40px"}} >Casual Couture</Text>
+                    <Text fontFamily="Alumni Sans, sans-serif" fontWeight={1000} fontSize={{base:'30px',md:'50px'}}>Casual Couture</Text>
                 </div>
             </Link>
             <SearchBarAdmin/>
